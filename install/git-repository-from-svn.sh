1ed801468686aec00e317fc170414962c3788db3
Ben <ben@tergology.com>
2016-12-14 22:44:36 +0100

    Fix syntax error in condition

--- install/git-repository-from-svn.sh
+++ install/git-repository-from-svn.sh
@@ -19,7 +19,7 @@
 #
 # Usage: git-repository-from-svn.sh project svn_url git_url
 
-if [ -z "${GIT_SCRIPTS}" ] || [ -z "${GIT_SVN_SYNC_BASE}" ] ] ; then
+if [ -z "${GIT_SCRIPTS}" ] || [ -z "${GIT_SVN_SYNC_BASE}" ] ; then
     echo "The following variables are required for the synchronization to work: GIT_SCRIPTS GIT_SVN_SYNC_BASE"
     exit 1
 fi
